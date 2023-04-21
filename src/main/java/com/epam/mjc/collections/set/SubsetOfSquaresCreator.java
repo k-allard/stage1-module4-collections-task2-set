d9a28186dc8521462dd99469d3bb375aec63897a
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-04-21 22:20:14 +0400

    Done 3/3 tasks

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,16 @@
 package com.epam.mjc.collections.set;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NavigableSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        List<Integer> squaredSourceList = new ArrayList<>(sourceList);
+        squaredSourceList.replaceAll(i -> i * i);
+        NavigableSet<Integer> res = new TreeSet<>(squaredSourceList);
+        return res.subSet(lowerBound, true, upperBound, true);
     }
 }
